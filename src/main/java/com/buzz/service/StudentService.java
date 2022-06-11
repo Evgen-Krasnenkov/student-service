1ae81184a3072d202cd1a99d0f99a37cb2088947
evgenkras <krasnenkov.evgen@gmail.com>
2022-06-11 19:20:31 -0400

    "commit"

--- src/main/java/com/buzz/service/StudentService.java
+++ src/main/java/com/buzz/service/StudentService.java
@@ -1,18 +1,22 @@
 package com.buzz.service;
 
+import com.buzz.dto.AddressResponse;
 import com.buzz.dto.CreateStudentRequest;
 import com.buzz.dto.StudentResponse;
 import com.buzz.model.Student;
 import com.buzz.repos.StudentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class StudentService {
 
 	StudentRepository studentRepository;
+	WebClient webClient;
 
-	public StudentService(StudentRepository studentRepository) {
+	public StudentService(StudentRepository studentRepository, WebClient webClient) {
 		this.studentRepository = studentRepository;
+		this.webClient = webClient;
 	}
 
 	public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
@@ -24,12 +28,19 @@ public class StudentService {
 		
 		student.setAddressId(createStudentRequest.getAddressId());
 		student = studentRepository.save(student);
-
-		return new StudentResponse(student);
+		StudentResponse studentResponse = new StudentResponse(student);
+		studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+		return studentResponse;
 	}
 	
 	public StudentResponse getById (long id) {
+		Student student = studentRepository.findById(id).orElseThrow();
+		StudentResponse studentResponse = new StudentResponse(student);
+		studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+		return studentResponse;
+	}
 
-		return new StudentResponse(studentRepository.findById(id).orElseThrow());
+	public AddressResponse getAddressById(Long addressId){
+		return webClient.get().uri("/getById/" + addressId).retrieve().bodyToMono(AddressResponse.class).block();
 	}
 }
